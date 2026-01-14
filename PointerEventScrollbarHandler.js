933a6b6f22e9dbc0cc6524638dc16f552812a82d
mins01 <mins01@inuscomm.co.kr>
2026-01-14 15:56:03 +0900

    PointerEventHandler 에서 상속 받는 것 제거.

--- PointerEventScrollbarHandler.js
+++ PointerEventScrollbarHandler.js
@@ -1,4 +1,4 @@
-class PointerEventScrollbarHandler extends PointerEventHandler{
+class PointerEventScrollbarHandler{
     axis="both";
     invert=true;
     scrollTop0 = 0;
@@ -6,7 +6,7 @@ class PointerEventScrollbarHandler extends PointerEventHandler{
     scrollbar = null
     multiplier = 1;
     constructor(target,options={}){
-        super(target)
+        this.target = target;
         this.axis = options.axis??'both';
         this.invert = options.invert??'both';
         this.scrollbar = options.scrollbar??target;
@@ -18,14 +18,14 @@ class PointerEventScrollbarHandler extends PointerEventHandler{
         this.target.addEventListener('pointermove.peh',this.scrollPointermove)
         this.target.addEventListener('pointerup.peh',this.scrollPointerup)
         this.target.addEventListener('pointercancel.peh',this.scrollPointerup)
-        this.target.addEventListener('pointerleave.peh',this.scrollPointerup)
+        // this.target.addEventListener('pointerleave.peh',this.scrollPointerup)
     }
     off(){
         this.target.removeEventListener('pointerdown.peh',this.scrollPointerdown)
         this.target.removeEventListener('pointermove.peh',this.scrollPointermove)
         this.target.removeEventListener('pointerup.peh',this.scrollPointerup)
         this.target.removeEventListener('pointercancel.peh',this.scrollPointerup)
-        this.target.removeEventListener('pointerleave.peh',this.scrollPointerup)
+        // this.target.removeEventListener('pointerleave.peh',this.scrollPointerup)
     }
     scrollPointerdown = (event)=>{
         this.scrollLeft0 = this.scrollbar.scrollLeft;
